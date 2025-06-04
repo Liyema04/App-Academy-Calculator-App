3a744455cc0f60baa583486f7e2f35086f0da2d8
Liyema Mkebe <liyema.mkebe15@gmail.com>
2025-06-04 14:46:04 +0200

    Week 5 [Section 1 L5 - 7] -> Complete!
    - The Calculator App
    
     Built:
    
    - Operator Button Event Handler
    - Equals Button Event Handler
    - Styling

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // (Week 4) Section 1: The Calculator App, Lesson 3: Removing the Zero in the Display 
 
 let newLine = true; // Boolean variable. Determines if the next thing the user types should be on a new line
+let value1;
+let currentOperator;
+
 
 // (Week 4) Section 1: The Calculator App, Lesson 2: Event Handler
 
@@ -21,5 +24,41 @@ function digitBtnPressed(button) {
 // Event handler for when AC button is pressed
 function btnACPressed() {
     document.getElementById("inputBox").value = 0;
+    newLine = true;
+}
+
+// (Week 4) Section 1: The Calculator App, Lesson 5: Operator Button Event Handler
+
+// Event handler for when Operator button is pressed
+function operatorBtnPressed(operator) { 
+    currentOperator = operator;
+    value1 = parseInt(document.getElementById("inputBox").value);
+    newLine = true;
+}
+
+// (Week 4) Section 1: The Calculator App, Lesson 6: Equals Button Event Handler
+
+// Event handler for Equals-To button
+function equalsBtnPressed() {
+    let value2 = parseInt(document.getElementById("inputBox").value);
+    let finalTotal;
+
+    switch(currentOperator) {
+        case "+":
+            finalTotal = value1 + value2;
+            break;
+        case "-":
+            finalTotal = value1 - value2;
+            break;
+        case "/":
+            finalTotal = value1 / value2;
+            break;
+        case "*":
+            finalTotal = value1 * value2;
+            break;        
+    }
+    document.getElementById("inputBox").value = finalTotal;
+
+    value1 = 0;
     newLine = true;
 }
